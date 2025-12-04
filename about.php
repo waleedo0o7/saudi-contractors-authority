ea627afddcb65496e202cc62904d602d39277c4c
mostafa <mostafabarcabarca@gmail.com>
2025-12-04 19:59:26 +0200

    create about page

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include '_header.php'; ?>
+<?php include '_top-header.php'; ?>
+
+<div class="page inner-page about">
+
+    <h1> hello i am about </h1>
+
+</div> <!-- page -->
+
+<?php include '_bottom-footer.php'; ?>
+<?php include '_footer.php'; ?>
